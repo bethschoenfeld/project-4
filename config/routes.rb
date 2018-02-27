47a7fcd673ff0803417d363992a04624ced08049
bethschoenfeld <bethingraham3@gmail.com>
2018-02-27 14:11:35 -0500

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users do
+      resources :events 
+    end
+  end
+
+  namespace :api do
+    resources :innovators do
+      resources :events
+    end
+  end
+
 end
+
