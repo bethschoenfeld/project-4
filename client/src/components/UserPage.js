f91e2aa0088f73325f4028808ccfc222fe48e68d
bethschoenfeld <bethingraham3@gmail.com>
2018-02-28 12:12:31 -0500

    Added userpage component

---
+++ client/src/components/UserPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components'
+
+class UserPage extends Component {
+  render() {
+    return (
+      <div>
+        Hello from UserPage
+      </div>
+    );
+  }
+}
+
+export default UserPage;
