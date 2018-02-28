3f6fd816b051fa4ddfa227de9b0521d0d8c2ffe7
bethschoenfeld <bethingraham3@gmail.com>
2018-02-28 09:09:23 -0500

    fixed my db. adjusted events table to make more sense

--- db/migrate/20180227165534_create_events.rb
+++ db/migrate/20180227165534_create_events.rb
@@ -1,8 +1,8 @@
 class CreateEvents < ActiveRecord::Migration[5.1]
   def change
     create_table :events do |t|
-      # t.enum :type
-      t.string :description
+      t.string :workshop
+      t.string :oneonone
       t.references :user, foreign_key: true
       t.references :innovator, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,8 @@ ActiveRecord::Schema.define(version: 20180227165534) do
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
-    t.string "description"
+    t.string "workshop"
+    t.string "oneonone"
     t.bigint "user_id"
     t.bigint "innovator_id"
     t.datetime "created_at", null: false
